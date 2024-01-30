6fc070678728a146d0ec5d8a5d732858ddfd4ec2
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2024-01-30 18:53:33 +0100

    Finish

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,10 @@
 
 int main(void) {
   try {
-    Form form("mohamed", 1, 30);
-    Bureaucrat bureau("hassan", 9);
+    Form form("mohamed", 10, 30);
+    Bureaucrat boss("boss", 5);
 
-    bureau.signForm(form);
+    boss.signForm(form);
 
   } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
